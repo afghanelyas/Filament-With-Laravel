24a0a698057f5a424a4b7871f298830ab13dde8d
afghanelyas <afghan.elyas1@gmail.com>
2023-07-29 07:05:41 +0430

    create a route for admin access panel

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['prefix' => 'admin', 'middleware' => 'admin'], function () {
+    // Your admin routes here
+    Route::get('/admin', function () {
+        return 'Welcome Admin';
+    });
+});
